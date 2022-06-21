200a79297584bcecebfc880c4109cbb56db3ce04
gowshi <gowsigowsalyait923@gmail.com>
2022-06-20 23:40:27 -0700

    git sample 1

---
+++ src/gitpack/GitClass.java
@@ -0,0 +1,16 @@
+package gitpack;
+
+public class GitClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		
+		
+		System.out.println("Hi....I'm github");
+		System.out.println("Hellooo");
+		
+	}
+
+}
