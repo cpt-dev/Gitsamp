5e328afb6e9d9978d3798b1dd421d250a7600a50
gowshi <gowsigowsalyait923@gmail.com>
2022-06-21 01:59:10 -0700

    how r u

--- src/gitpack/GitClass.java
+++ src/gitpack/GitClass.java
@@ -3,13 +3,14 @@ package gitpack;
 public class GitClass {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// TODO Auto-generated method tub
 
 		
 		
 		
 		System.out.println("Hi....I'm github");
 		System.out.println("Hellooo");
+		System.out.println("How are you?");
 		
 	}
 
