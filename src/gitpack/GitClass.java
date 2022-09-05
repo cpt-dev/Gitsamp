7032a3af2ad37faacae0f5b733b12bfd4af4cd7f
gowshi77gowshi <103502085+gowshi77gowshi@users.noreply.github.com>
2022-09-05 03:31:41 -0700

    commit
    AB#6

--- src/gitpack/GitClass.java
+++ src/gitpack/GitClass.java
@@ -11,7 +11,7 @@ public class GitClass {
 		System.out.println("Hi....I'm github");
 		System.out.println("Hellooo");
 		System.out.println("How are you?");
-		System.out.println("sounds good");
+		System.out.println("sounds good1");
 		
 		
 	}
